d9dc8ede910e00828e88cdb3d0651804d9ab9149
Ulia Karpova <chelovek-e-95@mail.ru>
2024-11-21 09:16:03 +0300

    hw-4/3

--- src/components/ProgressScrollBar.jsx
+++ src/components/ProgressScrollBar.jsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 export const ProgressScrollBar = () => {
   const [width, setWidth] = useState("0");
-  const onScroll = () => {
-    const scrollPx = document.documentElement.scrollTop;
-    const winHeightPx =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
-    const scrolled = `${(scrollPx / winHeightPx) * 100}%`;
-    setWidth(scrolled);
-  };
 
   useEffect(() => {
+    const onScroll = () => {
+      const scrollPx = document.documentElement.scrollTop;
+      const winHeightPx =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      const scrolled = `${(scrollPx / winHeightPx) * 100}%`;
+      setWidth(scrolled);
+    };
     window.addEventListener("scroll", onScroll);
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
